4940825a51ac69b11f80980ba2c609cc17f88194
Stephan Spiegel <stephan@stephanspiegel.com>
2022-03-24 05:06:33 -0400

    Limit cspell to certain filetypes

--- nvim/lua/stephanspiegel/pluginconfigs/null-ls.lua
+++ nvim/lua/stephanspiegel/pluginconfigs/null-ls.lua
@@ -11,7 +11,9 @@ local sources = {
     null_ls.builtins.formatting.stylelint,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.xmllint,
-    null_ls.builtins.diagnostics.cspell,
+    null_ls.builtins.diagnostics.cspell.with({
+        filtetypes = { 'html', 'json', 'yaml', 'markdown', 'text' }
+    }),
     null_ls.builtins.diagnostics.codespell,
     null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.code_actions.gitsigns,
