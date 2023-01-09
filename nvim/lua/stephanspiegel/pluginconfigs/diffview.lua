d9b89b507f28c5bd7888cc6c33aff7b4cd70eb0c
Stephan Spiegel <stephanspiegel@Sams-MBP.maine.rr.com>
2023-01-09 14:42:04 -0500

    Update diffview plugin config

--- nvim/lua/stephanspiegel/pluginconfigs/diffview.lua
+++ nvim/lua/stephanspiegel/pluginconfigs/diffview.lua
@@ -32,20 +32,22 @@ diffview.setup {
       height = 16,
     },
     log_options = {
-      single_file = {
-        max_count = 256,      -- Limit the number of commits
-        follow = true,       -- Follow renames (only for single file)
-        all = false,          -- Include all refs under 'refs/' including HEAD
-        merges = false,       -- List only merge commits
-        no_merges = false,    -- List no merge commits
-        reverse = false,      -- List commits in reverse order
-      },
-      multi_file = {
-        max_count = 256,      -- Limit the number of commits
-        all = false,          -- Include all refs under 'refs/' including HEAD
-        merges = false,       -- List only merge commits
-        no_merges = false,    -- List no merge commits
-        reverse = false,      -- List commits in reverse order
+      git = {
+        single_file = {
+          max_count = 256,      -- Limit the number of commits
+          follow = true,       -- Follow renames (only for single file)
+          all = false,          -- Include all refs under 'refs/' including HEAD
+          merges = false,       -- List only merge commits
+          no_merges = false,    -- List no merge commits
+          reverse = false,      -- List commits in reverse order
+        },
+        multi_file = {
+          max_count = 256,      -- Limit the number of commits
+          all = false,          -- Include all refs under 'refs/' including HEAD
+          merges = false,       -- List only merge commits
+          no_merges = false,    -- List no merge commits
+          reverse = false,      -- List commits in reverse order
+        }
       }
     },
   },
