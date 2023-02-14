e58d1bf103b509cbe5e7bd725f657be8c169e8ec
Stephan Spiegel <sspiegel@perchenergy.com>
2023-02-14 17:35:17 -0500

    Remove deprecated config items

--- nvim/lua/stephanspiegel/pluginconfigs/nvim-tree.lua
+++ nvim/lua/stephanspiegel/pluginconfigs/nvim-tree.lua
@@ -5,12 +5,6 @@ nvim_tree.setup {
   respect_buf_cwd = true,
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
