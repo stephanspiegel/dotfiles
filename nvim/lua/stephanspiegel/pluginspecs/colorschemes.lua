c4a0ad74c3186b2e62a348ab0b923302988d474b
Stephan Spiegel <stephan@stephanspiegel.com>
2023-09-07 14:48:38 -0400

    Move colorscheme specs to own file

---
+++ nvim/lua/stephanspiegel/pluginspecs/colorschemes.lua
@@ -0,0 +1,58 @@
+
+-- ╒══════════════════════════════════════════════════════════╕
+--                     Theme Specifications
+-- ╘══════════════════════════════════════════════════════════╛
+local colorscheme_specifications = {
+  { "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  { "junegunn/seoul256.vim" },
+  { "mhartington/oceanic-next" },
+  { "morhetz/gruvbox" },
+  { "rafamadriz/neon" },
+  { "rakr/vim-one" },
+  { "rakr/vim-two-firewatch" },
+  { "rockerBOO/boo-colorscheme-nvim" },
+  { "sainnhe/edge" },
+  { "sainnhe/everforest" },
+  { "sainnhe/forest-night" },
+  { "sainnhe/gruvbox-material" },
+  { "sainnhe/sonokai" },
+  { "savq/melange" },
+  { "smallwat3r/vim-hashpunk-sw" },
+  { "theniceboy/nvim-deus" },
+  { "xstrex/FireCode.vim" },
+  { "ayu-theme/ayu-vim" },
+  { "bluz71/vim-moonfly-colors" },
+  { "mbbill/desertEx" },
+  { "shaunsingh/oxocarbon.nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "arcticicestudio/nord-vim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({ colorscheme = "neon_latte" })
+    end,
+  },
+  { "embark-theme/vim",
+    name = "embark"
+  },
+  { "eddyekofo94/gruvbox-flat.nvim" },
+  {
+    "rose-pine/neovim",
+    config = function()
+      require("rose-pine").setup({
+        dark_variant = "moon",
+      })
+    end,
+  },
+  { "https://gitlab.com/protesilaos/tempus-themes-vim" },
+}
+
+return colorscheme_specifications
