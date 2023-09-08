17b5671cfd3876e71b32eebef3223d2fac9a563e
Stephan Spiegel <stephan@stephanspiegel.com>
2023-09-08 09:28:29 -0400

    Add neorg plugin

--- nvim/lua/stephanspiegel/pluginspecs/init.lua
+++ nvim/lua/stephanspiegel/pluginspecs/init.lua
@@ -95,6 +95,26 @@ local plugin_specifications = {
       require("mind").setup()
     end
   },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
   -- ╭──────────────────────────────────────────────────────────╮
   -- │                        Navigation                        │
   -- ╰──────────────────────────────────────────────────────────╯
