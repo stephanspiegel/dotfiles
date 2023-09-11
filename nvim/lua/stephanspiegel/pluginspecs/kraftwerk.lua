988654cc4018218ad11b17dd3ada6993c0929f6a
Stephan Spiegel <stephan@stephanspiegel.com>
2023-09-11 07:18:13 -0400

    Move kraftwerk config to spec

--- nvim/lua/stephanspiegel/pluginspecs/kraftwerk.lua
+++ nvim/lua/stephanspiegel/pluginspecs/kraftwerk.lua
@@ -1,11 +1,15 @@
 return {
   {
     "stephanspiegel/kraftwerk.nvim",
-    dir = "~/Projects/vim/kraftwerk.nvim",
+    -- dir = "~/Projects/vim/kraftwerk.nvim",
     dev = false,
-    config = function()
-      require("stephanspiegel.pluginconfigs.kraftwerk")
-    end,
-    ft = "apexcode"
+    config = true,
+    ft = "apexcode",
+    keys = {
+      {'gqq', 'vap:ForceDataSoqlQuery csv<CR>', mode = 'n', desc = 'Run current paragraph as SOQL query'},
+      {'gee', 'vap:ForceApexExecute<CR>', mode = 'n', desc = 'Run current paragraph as Anonymous Apex'},
+      {'gqq', ":'<,'>ForceDataSoqlQuery csv<CR>", mode = 'v', desc = 'Run selected text as SOQL query'},
+      {'gee', ":'<,'>ForceApexExecute<CR>", mode = 'v', desc = 'Run selected text as Anonymous Apex'}
+    }
   }
 }
