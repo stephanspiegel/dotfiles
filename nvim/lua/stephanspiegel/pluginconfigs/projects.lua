f6ce74d65dc519d5900943dd5a62205dfd25b408
Stephan Spiegel <stephan@stephanspiegel.com>
2023-09-11 07:18:45 -0400

    Set chdir to silent

--- nvim/lua/stephanspiegel/pluginconfigs/projects.lua
+++ nvim/lua/stephanspiegel/pluginconfigs/projects.lua
@@ -5,7 +5,7 @@ projects.setup {
   show_hidden = true,
   -- When set to false, you will get a message when project.nvim changes your
   -- directory.
-  silent_chdir = false,
+  silent_chdir = true,
 }
 local telescope_status_ok, telescope = pcall(require, 'telescope')
 if telescope_status_ok then
