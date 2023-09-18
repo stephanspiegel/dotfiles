fabcf1459f83804c5a1ea21e11b7055391a4fb4d
Stephan Spiegel <sspiegel@perchenergy.com>
2023-09-18 12:47:10 -0400

    Add rest-nvim plugin

--- nvim/lua/stephanspiegel/pluginspecs/init.lua
+++ nvim/lua/stephanspiegel/pluginspecs/init.lua
@@ -111,6 +111,7 @@ local plugin_specifications = {
         },
       }
     end,
+    cmd = { 'Neorg' }
   },
   -- ╭──────────────────────────────────────────────────────────╮
   -- │                        Navigation                        │
@@ -200,11 +201,59 @@ local plugin_specifications = {
     "chrisbra/csv.vim",
     ft = { "csv" }
   },
-  { 
+  {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
     init = function() vim.g.mkdp_filetypes = { "markdown" } end,
     ft = { "markdown" },
+  },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        -- Open request results in a horizontal split
+        result_split_horizontal = true,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = false,
+          -- show the generated curl command in case you want to launch
+          -- the same request via the terminal (can be verbose)
+          show_curl_command = false,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = "jq",
+            html = function(body)
+              return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+            end
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      },
+    ft = "http",
+    keys = {
+      {'<Leader>hh', '<Plug>RestNvim', desc = 'Run HTTP request under cursor'},
+      {'<Leader>hp', '<Plug>RestNvimPreview', desc = 'Preview the request cURL command'},
+      {'<M-r>', '<Plug>RestNvimLast', desc = 'Rerun the last request'},
+    },
+    cmd = { "RestNvim", "RestNvimPreview", "RestNvimLast" }
   },
   -- ╭──────────────────────────────────────────────────────────╮
   -- │                        Completion                        │
