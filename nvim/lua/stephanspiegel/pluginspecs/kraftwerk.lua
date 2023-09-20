1197f7eaab7e0459a88093505433feed68a21ccf
Stephan Spiegel <sspiegel@perchenergy.com>
2023-09-20 15:00:18 -0400

    Fix up salesforce tools

--- nvim/lua/stephanspiegel/pluginspecs/kraftwerk.lua
+++ nvim/lua/stephanspiegel/pluginspecs/kraftwerk.lua
@@ -6,10 +6,10 @@ return {
     config = true,
     ft = "apexcode",
     keys = {
-      {'gqq', 'vap:ForceDataSoqlQuery csv<CR>', mode = 'n', desc = 'Run current paragraph as SOQL query'},
-      {'gee', 'vap:ForceApexExecute<CR>', mode = 'n', desc = 'Run current paragraph as Anonymous Apex'},
-      {'gqq', ":'<,'>ForceDataSoqlQuery csv<CR>", mode = 'v', desc = 'Run selected text as SOQL query'},
-      {'gee', ":'<,'>ForceApexExecute<CR>", mode = 'v', desc = 'Run selected text as Anonymous Apex'}
+      {'gqq', 'mxvap:ForceDataSoqlQuery csv<CR>`x', mode = 'n', desc = 'Run current paragraph as SOQL query'},
+      {'gee', 'mxvap:ForceApexExecute<CR>`x', mode = 'n', desc = 'Run current paragraph as Anonymous Apex'},
+      {'gqq', ":'<,'>ForceDataSoqlQuery csv<CR>gv", mode = 'v', desc = 'Run selected text as SOQL query'},
+      {'gee', ":'<,'>ForceApexExecute<CR>gv", mode = 'v', desc = 'Run selected text as Anonymous Apex'}
     }
   }
 }
