943581f403bee8508967f9b9bf7aa95d42329ca6
Stephan Spiegel <stephan@stephanspiegel.com>
2023-09-23 16:35:02 -0400

    Switch to veil for startup plugin

---
+++ nvim/lua/stephanspiegel/pluginspecs/veil.lua
@@ -0,0 +1,59 @@
+return {
+  'willothy/veil.nvim',
+  lazy = false,
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim"
+  },
+  opts = function()
+    local builtin = require("veil.builtin")
+    return {
+      sections = {
+        builtin.sections.animated(builtin.headers.frames_nvim, {
+          hl = { fg = "#5de4c7" },
+        }),
+        builtin.sections.buttons({
+          {
+            icon = "",
+            text = "Find Files",
+            shortcut = "f",
+            callback = function()
+              require("telescope.builtin").find_files()
+            end,
+          },
+          {
+            icon = "",
+            text = "Find Word",
+            shortcut = "w",
+            callback = function()
+              require("telescope.builtin").live_grep()
+            end,
+          },
+          {
+            icon = "",
+            text = "Buffers",
+            shortcut = "b",
+            callback = function()
+              require("telescope.builtin").buffers()
+            end,
+          },
+          {
+            icon = "",
+            text = "Config",
+            shortcut = "c",
+            callback = function()
+              require("telescope").extensions.file_browser.file_browser({
+                path = vim.fn.stdpath("config"),
+              })
+            end,
+          },
+        }),
+        builtin.sections.oldfiles(),
+      },
+      mappings = {},
+      startup = true,
+      listed = false
+    }
+  end
+}
