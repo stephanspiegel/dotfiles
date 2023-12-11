0cf01eece567648d4733ca195eb501b3665efb36
Stephan Spiegel <sspiegel@perchenergy.com>
2023-12-11 11:33:07 -0500

    Fix notification dismisser

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,12 +6,12 @@ yabai = require("yabai")
 caps2esc = require("caps2esc")
 
 -- Close all visible notifications in Notification Center.
-hs.hotkey.bind({"ctrl", "cmd"}, "delete", function()
+hs.hotkey.bind({"ctrl", "cmd"}, "n", function()
   hs.task
     .new("/usr/bin/osascript", nil, {
       "-l",
       "JavaScript",
-      os.getenv("HOME") .. "/.config/hammerspoon/jxa/close_notifications.js",
+      os.getenv("HOME") .. "/.hammerspoon/close_notifications.js",
     })
     :start()
 end)
