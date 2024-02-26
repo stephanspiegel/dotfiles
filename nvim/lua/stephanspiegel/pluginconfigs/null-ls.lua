8c0fff689894f5dbbddb81f70116713a92404f5c
Stephan Spiegel <sspiegel@perchenergy.com>
2024-02-26 12:58:13 -0500

    Remove deprecated applications

--- nvim/lua/stephanspiegel/pluginconfigs/null-ls.lua
+++ nvim/lua/stephanspiegel/pluginconfigs/null-ls.lua
@@ -4,8 +4,6 @@ local pmd = require'stephanspiegel.pluginconfigs.pmd'.pmd
 
 local sources = {
     builtins.formatting.codespell,
-    builtins.formatting.eslint_d,
-    builtins.formatting.fixjson,
     builtins.formatting.prettierd.with({
         extra_filetypes = { 'apex', 'apexcode' }
     }),
@@ -13,15 +11,12 @@ local sources = {
     builtins.formatting.shfmt,
     builtins.formatting.stylelint,
     builtins.formatting.stylua,
-    builtins.formatting.xmllint,
     builtins.diagnostics.cspell.with({
         filetypes = { 'html', 'json', 'yaml', 'markdown', 'text' }
     }),
     builtins.diagnostics.codespell,
-    builtins.diagnostics.shellcheck,
     builtins.diagnostics.write_good,
     builtins.code_actions.gitsigns,
-    builtins.code_actions.shellcheck,
     pmd
 }
 
