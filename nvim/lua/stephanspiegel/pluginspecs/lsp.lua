3fba97094ad0ec6d0bcf4418e550a271fc4cee47
Stephan Spiegel <sspiegel@perchenergy.com>
2024-03-21 06:17:53 -0400

    Add lsp_lines plugin

--- nvim/lua/stephanspiegel/pluginspecs/lsp.lua
+++ nvim/lua/stephanspiegel/pluginspecs/lsp.lua
@@ -88,4 +88,16 @@ return {
     end,
     dependencies = { "mfussenegger/nvim-dap" }
   },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function ()
+        -- Disable virtual_text since it's redundant due to lsp_lines.
+        vim.diagnostic.config({
+          virtual_text = false,
+        })
+        require("lsp_lines").setup()
+    end,
+    dependencies = { "neovim/nvim-lspconfig" },
+    event = { "BufReadPost", "BufNewFile" },
+  }
 }
