c530fff96a778dd037edb608623d8627cdcaf403
Stephan Spiegel <stephan@stephanspiegel.com>
2024-04-17 09:22:51 -0400

    Use non-icons in nvim-tree

--- nvim/lua/stephanspiegel/pluginspecs/nvim-tree.lua
+++ nvim/lua/stephanspiegel/pluginspecs/nvim-tree.lua
@@ -6,48 +6,48 @@ return {
     {'<Leader>e', ':NvimTreeToggle<cr>', desc = 'Toggle NvimTree'}
   },
   opts = {
-      sync_root_with_cwd = true,
-      respect_buf_cwd = true,
-      disable_netrw = true,
-      hijack_netrw = true,
-      open_on_tab = false,
-      hijack_cursor = false,
-      update_cwd = true,
-      diagnostics = {
-          enable = true,
-          icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-          },
-        },
-  update_focused_file = {
+    sync_root_with_cwd = true,
+    respect_buf_cwd = true,
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = true,
+    diagnostics = {
+      enable = true,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      },
+    },
+    update_focused_file = {
       enable = true,
       update_cwd = true,
       update_root = true,
       ignore_list = {},
     },
-      git = {
+    git = {
       enable = true,
       ignore = true,
       timeout = 500,
     },
-      view = {
+    view = {
       width = 30,
       side = "left",
       number = false,
       relativenumber = false,
     },
-      actions = {
+    actions = {
       change_dir = {
         global = true,
       },
     },
-      renderer = {
-    icons = {
-      glyphs = nonicons_extention.glyphs,
-    },
+    renderer = {
+      icons = {
+        glyphs = nonicons_extention.glyphs,
+      },
     }
   },
 }
