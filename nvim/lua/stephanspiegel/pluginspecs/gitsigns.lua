ccb98df0235ac30c4b939e479b8dc14af585ea9d
Stephan Spiegel <sspiegel@perchenergy.com>
2024-06-25 10:15:17 -0400

    Remove deprecated configs

--- nvim/lua/stephanspiegel/pluginspecs/gitsigns.lua
+++ nvim/lua/stephanspiegel/pluginspecs/gitsigns.lua
@@ -4,11 +4,11 @@ return  {
   event = { "BufReadPost", "BufNewFile" },
   opts = {
     signs = {
-      add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "▍", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "▁", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = { hl = "GitSignsDelete", text = "▔", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      changedelete = { hl = "GitSignsChange", text = "▍", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      add = { text = "▍" },
+      change = { text = "▍" },
+      delete = { text = "▁" },
+      topdelete = { text = "▔" },
+      changedelete = { text = "▍" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -26,9 +26,6 @@ return  {
       delay = 1000,
       ignore_whitespace = false,
     },
-    current_line_blame_formatter_opts = {
-      relative_time = false,
-    },
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
@@ -40,9 +37,6 @@ return  {
       relative = "cursor",
       row = 0,
       col = 1,
-    },
-    yadm = {
-      enable = false,
     },
     on_attach = function(bufnr)
       local function map(mode, lhs, rhs, opts)
