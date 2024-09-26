95627d7a8194e7650166b66093a8fcc067dfd846
Stephan Spiegel <sspiegel@perchenergy.com>
2024-09-26 10:14:08 -0400

    Remove deprecated settings

--- nvim/lua/stephanspiegel/pluginspecs/init.lua
+++ nvim/lua/stephanspiegel/pluginspecs/init.lua
@@ -56,14 +56,12 @@ local plugin_specifications = {
     "rmagatti/auto-session",
     config = function()
       require("auto-session").setup({
+        auto_restore = false,
+        auto_restore_last_lession = false,
+        auto_session_last_session_dir = "",
+        enabled = true,
         log_level = "info",
-        auto_session_last_session_dir = '',
-        auto_session_enable_last_session = false,
-        auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
-        auto_session_enabled = true,
-        auto_save_enabled = nil,
-        auto_restore_enabled = false,
-        auto_session_suppress_dirs = nil,
+        root_dir = vim.fn.stdpath("data") .. "/sessions/"
       })
     end,
     lazy = false
