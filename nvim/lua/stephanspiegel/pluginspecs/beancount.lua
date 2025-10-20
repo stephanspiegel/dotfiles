899d7cc4085c57586d791d8c24b522f3b1a75431
Stephan Spiegel <stephan@stephanspiegel.com>
2025-10-20 12:26:06 +0200

    Fix beancount setup

--- nvim/lua/stephanspiegel/pluginspecs/beancount.lua
+++ nvim/lua/stephanspiegel/pluginspecs/beancount.lua
@@ -5,34 +5,30 @@
 return {
   {
     "polarmutex/beancount.nvim",
-    ft="beancount"
+    ft="beancount",
+    config = function ()
+        vim.api.nvim_create_autocmd( {'BufRead', 'BufNewFile'},
+          {
+            pattern = {'*.beancount'},
+            command = "python3 import sys; sys.path.append('<venv path>/lib/python3.11/site-packages'); import beancount; sys.path.pop()"
+          }
+      )
+    end
   },
   {
     "crispgm/cmp-beancount",
+    dependencies = { "jmcantrell/vim-virtualenv" },
     ft="beancount"
   },
   {
     "nathangrigg/vim-beancount",
     ft="beancount",
+    dependencies = { "jmcantrell/vim-virtualenv" },
     config = function ()
       vim.g.beancount_separator_col = 70
     end,
     keys={
       { '=', ":'<,'>AlignCommodity<CR>", mode = 'v', desc = 'Align on decimal point'},
-      {
-        '<C-T>',
-        '<ESC>:lua require("flash").toggle(false)<CR>?[0-9.]\\+<CR>c//e<CR><C-R>=luaeval("require \'stephanspiegel.functions\'.taxify(_A)", str2float(@"))<CR><ESC>:nohlsearch<CR>:lua require("flash").toggle(true)<CR>a',
-        mode = 'i',
-        desc = 'Add Maine state sales tax',
-        ft = 'beancount'
-      },
-      {
-        '<C-T>',
-        '"xygvc<C-R>=luaeval("require \'stephanspiegel.functions\'.taxify(_A)", str2float(@x))<CR><ESC>',
-        mode = 'v',
-        desc = 'Add Maine state sales tax',
-        ft = 'beancount'
-      }
     }
   },
 }
